37b4994ee178e38c58da67de32319e54fe4637ba
BelloForte <dinomudrovcic@gmail.com>
2019-05-22 21:17:48 +0200

    Add files via upload

---
+++ FinAppDB.sql
@@ -0,0 +1,20 @@
+use FinAppDB
+go
+
+create table user_table(
+	id_user			bigint			not null	primary key,
+	name			nvarchar(max)	null,
+	surname			nvarchar(max)	null,
+	account_status	float			null
+)
+go
+
+create table transaction_table(
+	id_transaction	bigint			not null	primary key,
+	date_created	datetime		not null,
+	userid			bigint			not null	constraint userid foreign key references user_table(id_user),
+	operation		nvarchar(max)	not null,
+	amount			float			not null,
+	debtorid		bigint			null		constraint debtorid foreign key references user_table(id_user)
+)
+go
